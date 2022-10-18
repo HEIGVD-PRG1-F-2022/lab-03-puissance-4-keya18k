ab5d6fa181919992200bd100283f2dd39cb00bd5
dessasa <keyaspace@hotmail.fr>
2022-10-18 16:28:57 +0200

    ajout affichage

--- main.cpp
+++ main.cpp
@@ -43,17 +43,51 @@ void checkWinner ( vector<vector<CellType>> gameGrid) {
     }
 }
 
+char getPlayerChar(CellType type){
+    switch(type){
+        case PLAYER_ONE: return '1';
+        case PLAYER_TWO: return '2';
+        default : return ' ';
+    }
+}
+void addPiece(vector<vector<CellType>>& gameGrid, int colonneIndex, CellType type){
+    for(int y = gameGrid.size()-1; y >= 0; y--){
+        CellType currentType = gameGrid[y][colonneIndex];
+        if(currentType == EMPTY){
+            gameGrid[y][colonneIndex] = type;
+            return;
+        }
+    }
+}
+void display(vector<vector<CellType>> gameGrid){
+    for(int y = 0; y < gameGrid.size(); y++){
+        for(int c = 0; c < gameGrid[0].size()*4+1; c++){
+            cout << '-';
+        }
+        cout << endl;
+        for(int x = 0; x < gameGrid[0].size(); x++){
+            cout << '|' << " " << getPlayerChar(gameGrid[y][x]) << " ";
+        }
+        cout << '|' << endl;
+    }for(int c = 0; c < gameGrid[0].size()*4+1; c++){
+        cout << '-';
+    }
+}
 int main() {
     int colomns = 7;
     int lines = 6;
 
     vector<vector<CellType>> gameGrid (lines, vector<CellType> (colomns, EMPTY));
 
-    gameGrid[3] = {
+   /* gameGrid[3] = {
             EMPTY, EMPTY, EMPTY, PLAYER_ONE, PLAYER_ONE, PLAYER_ONE, PLAYER_ONE
-    };
-
+    };*/
+    addPiece(gameGrid, 0, PLAYER_ONE);
+    addPiece(gameGrid, 0, PLAYER_ONE);
+    display(gameGrid);
     checkWinner(gameGrid);
+
+
 
     return 0;
 }
