12ddd9e4f3f6f7116290458124d4f48591e0e76e
borishuu <boris.hutzli@heig-vd.ch>
2022-10-20 14:05:03 +0200

    column and line input implementation + tie

--- main.cpp
+++ main.cpp
@@ -6,32 +6,31 @@ using namespace std;
 const int WIN_COUNT = 4;
 
 enum CellType { PLAYER_ONE, PLAYER_TWO, EMPTY };
+enum GameEndedReason { PLAYER_WON, TIE };
 
+// gameEndedVerification
 bool checkWinner (const vector<vector<CellType>>& gameGrid) {
-    bool won = true;
     for (int y = 0; y < gameGrid.size(); y++) {
         for (int x = 0; x < gameGrid[0].size(); x++) {
+            bool won = true;
             CellType type = gameGrid[y][x];
 
             if (type == EMPTY) {
                 continue;
             }
             // horizontal right
-            {
-
-                for (int i = 1; i <= WIN_COUNT - 1; i++) {
-                    if (x + i >= gameGrid[0].size()) {
-                        won = false;
-                        break;
-                    }
-                    CellType nextCell = gameGrid[y][x + i];
-                    if (nextCell != type) {
-                        won = false;
-                        break;
-                    }
+            for (int i = 1; i <= WIN_COUNT - 1; i++) {
+                if (x + i >= gameGrid[0].size()) {
+                    won = false;
+                    break;
                 }
-
+                CellType nextCell = gameGrid[y][x + i];
+                if (nextCell != type) {
+                    won = false;
+                    break;
+                }
             }
+
             if(won){
                 return won;
             }
@@ -40,20 +39,16 @@ bool checkWinner (const vector<vector<CellType>>& gameGrid) {
 
 
             // vertical down
-            {
-
-                for (int i = 1; i <= WIN_COUNT - 1; i++) {
-                    if (y + i >= gameGrid.size()) {
-                        won = false;
-                        break;
-                    }
-                    CellType nextCell = gameGrid[y + i][x];
-                    if (nextCell != type) {
-                        won = false;
-                        break;
-                    }
+            for (int i = 1; i <= WIN_COUNT - 1; i++) {
+                if (y + i >= gameGrid.size()) {
+                    won = false;
+                    break;
                 }
-
+                CellType nextCell = gameGrid[y + i][x];
+                if (nextCell != type) {
+                    won = false;
+                    break;
+                }
             }
             if(won){
                 return won;
@@ -62,20 +57,16 @@ bool checkWinner (const vector<vector<CellType>>& gameGrid) {
             won = true;
 
             // diagonal right
-            {
-
-                for (int i = 1; i <= WIN_COUNT - 1; i++) {
-                    if (y + i >= gameGrid.size() || x + i >= gameGrid[0].size()) {
-                        won = false;
-                        break;
-                    }
-                    CellType nextCell = gameGrid[y + i][x + i];
-                    if (nextCell != type) {
-                        won = false;
-                        break;
-                    }
+            for (int i = 1; i <= WIN_COUNT - 1; i++) {
+                if (y + i >= gameGrid.size() || x + i >= gameGrid[0].size()) {
+                    won = false;
+                    break;
                 }
-
+                CellType nextCell = gameGrid[y + i][x + i];
+                if (nextCell != type) {
+                    won = false;
+                    break;
+                }
             }
             if(won){
                 return won;
@@ -83,43 +74,49 @@ bool checkWinner (const vector<vector<CellType>>& gameGrid) {
 
             won = true;
             // diagonal left
-            {
-
-                for (int i = 1; i <= WIN_COUNT - 1; i++) {
-                    if (y + i >= gameGrid.size() || x - i < 0) {
-                        won = false;
-                        break;
-                    }
-                    CellType nextCell = gameGrid[y + i][x - i];
-                    if (nextCell != type) {
-                        won = false;
-                        break;
-                    }
+            for (int i = 1; i <= WIN_COUNT - 1; i++) {
+                if (y + i >= gameGrid.size() || x - i < 0) {
+                    won = false;
+                    break;
                 }
+                CellType nextCell = gameGrid[y + i][x - i];
+                if (nextCell != type) {
+                    won = false;
+                    break;
+                }
+            }
 
+            if (won) {
+                return true;
             }
         }
     }
-    if (won) {
-        return true;
-    }
     return false;
 }
+bool checkTie(vector<vector<CellType>> gameGrid) {
+    for (int y = 0; y < gameGrid.size(); y++) {
+        for (int x = 0; x < gameGrid[0].size(); x++) {
+            if (gameGrid[y][x] == EMPTY) {
+                return false;
+            }
+        }
+    }
 
-char getPlayerChar(CellType type){
-    switch(type){
+    return true;
+}
+
+// gameInputOutput
+char getPlayerChar(CellType player){
+    switch(player){
         case PLAYER_ONE: return '1';
         case PLAYER_TWO: return '2';
         default : return ' ';
     }
 }
-void addPiece(vector<vector<CellType>>& gameGrid, int colonneIndex, CellType type){
-    for(int y = gameGrid.size()-1; y >= 0; y--){
-        CellType currentType = gameGrid[y][colonneIndex];
-        if(currentType == EMPTY){
-            gameGrid[y][colonneIndex] = type;
-            return;
-        }
+string getPlayerName(CellType player){
+    switch(player){
+        case PLAYER_ONE : return "Joueur 1";
+        case PLAYER_TWO : return "Joueur 2";
     }
 }
 void display(vector<vector<CellType>> gameGrid){
@@ -141,39 +138,84 @@ void display(vector<vector<CellType>> gameGrid){
     }
     cout << endl;
 }
+int getUserIntInput(int min, int max, string message) {
+    int input;
+    do {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(256, '\n');
+        }
+        cout << message;
+        cin >> input;
+        cout << endl;
+    } while (cin.fail() || input < min || input >= max);
+    return input;
+}
 
+// gameLogic
+void addPiece(vector<vector<CellType>>& gameGrid, int colonneIndex, CellType type){
+    for(int y = gameGrid.size()-1; y >= 0; y--){
+        CellType currentType = gameGrid[y][colonneIndex];
+        if(currentType == EMPTY){
+            gameGrid[y][colonneIndex] = type;
+            return;
+        }
+    }
+}
 void switchPlayerTurn(CellType& currentPlayerTurn){
     switch(currentPlayerTurn){
         case PLAYER_ONE : currentPlayerTurn = PLAYER_TWO; return;
         case PLAYER_TWO : currentPlayerTurn = PLAYER_ONE; return;
     }
 }
-int main() {
-    int colomns = 7;
-    int lines = 6;
-    CellType playerTurn = PLAYER_ONE;
-    vector<vector<CellType>> gameGrid (lines, vector<CellType> (colomns, EMPTY));
+void gameStart(){
+    const int MIN_COLUMNS_LINES = 4;
+    const int MAX_COLUMNS_LINES = 10;
 
-   /* bool gameRunning = true;
+
+    string gridSizeInputString = "Veuillez insérer le nombre de colonnes (entre " + to_string(MIN_COLUMNS_LINES) +
+                                 " et " + to_string(MAX_COLUMNS_LINES) + ") : ";
+
+    bool gameRunning = true;
+    CellType playerTurn = PLAYER_ONE;
+    GameEndedReason reason = PLAYER_WON;
+    int columns = getUserIntInput(MIN_COLUMNS_LINES, MAX_COLUMNS_LINES + 1, gridSizeInputString); // 7
+    int lines = getUserIntInput(MIN_COLUMNS_LINES, MAX_COLUMNS_LINES + 1, gridSizeInputString); // 6;
+    vector<vector<CellType>> gameGrid (lines, vector<CellType> (columns, EMPTY));
+
+
     display(gameGrid);
     do {
+        cout << "C'est au " << getPlayerName(playerTurn) << " de joueur." << endl;
 
-        int chosenColumn;
+        int chosenColumn = getUserIntInput(0, columns, "Veuillez inserer une valeure entre 0 et " +
+                                                       to_string(columns - 1) + " (inclus) : ");
 
-        do {
-            cout << "Veuillez inserer une valeure entre 0 et " << gameGrid[0].size() - 1 << " (inclus) : ";
-            cin >> chosenColumn;
-            cout << endl;
-        } while (chosenColumn < 0 || chosenColumn >= gameGrid[0].size());
         addPiece(gameGrid, chosenColumn, playerTurn);
-        switchPlayerTurn(playerTurn);
         gameRunning = !checkWinner(gameGrid);
         display(gameGrid);
-    } while (gameRunning); */
+        if (gameRunning) {
 
-    cout << playerTurn << " a gagne";
+            if (checkTie(gameGrid)) {
+                gameRunning = false;
+                reason = TIE;
+            }
+            switchPlayerTurn(playerTurn);
+        }
+    } while (gameRunning);
 
-    addPiece(gameGrid, 6, PLAYER_ONE);
+    switch (reason) {
+        case PLAYER_WON:
+            cout << getPlayerName(playerTurn) << " a gagne";
+        case TIE:
+            cout << "Personne a gagne";
+    }
+}
+
+int main() {
+    gameStart();
+
+    /*addPiece(gameGrid, 6, PLAYER_ONE);
     addPiece(gameGrid, 6, PLAYER_ONE);
     addPiece(gameGrid, 6, PLAYER_ONE);
     addPiece(gameGrid, 6, PLAYER_TWO);
@@ -190,7 +232,7 @@ int main() {
     bool winner = checkWinner(gameGrid);
     if (winner) {
         cout << "winner";
-    }
+    }*/
 
 
     return 0;
